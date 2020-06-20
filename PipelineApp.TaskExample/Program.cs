c83441f37f5319fa4e9e000004737eb4a63b9e92
Evgeny Ivanov <ivanov.john@gmail.com>
2020-06-20 23:41:32 +0700

    added example test task

---
+++ PipelineApp.TaskExample/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PipelineApp.TaskExample
+{
+    using System;
+    using System.Threading;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            var sleepTime = rand.Next(5000);
+            Console.WriteLine($"Started at {DateTime.UtcNow}, will be executing in {sleepTime}");
+            Thread.Sleep(sleepTime);
+            Console.WriteLine($"Finished at {DateTime.UtcNow}");
+        }
+    }
+}
